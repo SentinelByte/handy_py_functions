d4dfe1e43201c239059d8a07fda45f654ba34c32
SentinelByte <dandan894@gmail.com>
2024-06-01 17:45:42 +0300

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,14 @@
+# creating the list containing the desired object to sort 
+numbers = [3, 1, 4, 1, 5, 9, 2]
+letters = ['w', 'd', 'a', 'r']
+words = ['Hello', 'Cat', 'Dog',]
+
+# Apply the sorted() function for ordering the objects
+sorted_numbers = sorted(numbers)
+sorted_letters = sorted(letters)
+sorted_words = sorted(words)
+
+# print the results
+print(sorted_numbers)
+print(sorted_letters)
+print(sorted_words)
