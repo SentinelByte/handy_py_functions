52464206ccdff924ca1ca8b7eeae2eab1bdfbf80
SentinelByte <dandan894@gmail.com>
2024-06-01 17:48:15 +0300

    Create enumerate_objects.py

---
+++ enumerate.py
@@ -0,0 +1,10 @@
+# Enumerating elements of a list
+fruits = ['apple', 'banana', 'orange']
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+
+'''
+This code use the enumerate() function. 
+It adds a counter to an iterable object (such as a list) and returns it in the form of an enumerate object. 
+In this case, it's used to loop through the list fruits and print both the index and the corresponding fruit.
+'''
